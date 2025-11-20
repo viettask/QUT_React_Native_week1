f3ea81a05e09149c9d804885d516b63294b038e7
Viet Huynh Nguyen <49942312+viettask@users.noreply.github.com>
2025-11-20 23:14:45 +1000

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi Viet</Text>
+      <Text>Hi Viet Nguyen</Text>
       <StatusBar style="auto" />
     </View>
   );
